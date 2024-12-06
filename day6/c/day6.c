1e81f1ab3fe1de7b5c9f58957bafb480d183143d
Remi <remi.teyssier@edu.ece.fr>
2024-12-06 10:47:11 +0100

    feat: day6 part1

---
+++ day6/c/day6.c
@@ -0,0 +1,110 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define TARGET '^'
+#define WALL '#'
+typedef struct {
+  int dx;
+  int dy;
+} directions;
+
+directions Direction[4] = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+
+void find_initial_position(char **map, int num_rows, int num_cols, int *x,
+                           int *y) {
+  for (int i = 0; i < num_rows; i++) {
+    for (int j = 0; j < num_cols; j++) {
+      if (map[i][j] == TARGET) {
+        *x = i;
+        *y = j;
+        return;
+      }
+    }
+  }
+  printf("Target not find");
+}
+
+int guard_path(char **map, int num_rows, int num_cols) {
+  int x, y, next_x, next_y;
+  int index_direction = 3;
+  int add_guard_position = 0;
+
+  bool **visited = (bool **)malloc(num_rows * sizeof(bool *));
+  for (int i = 0; i < num_rows; i++) {
+    visited[i] = (bool *)malloc(num_cols * sizeof(bool));
+    for (int j = 0; j < num_cols; j++) {
+      visited[i][j] = false;
+    }
+  }
+
+  find_initial_position(map, num_rows, num_cols, &x, &y);
+  visited[x][y] = true;
+  add_guard_position++;
+
+  while (true) {
+    next_x = x + Direction[index_direction].dx;
+    next_y = y + Direction[index_direction].dy;
+
+    if (next_x < 0 || next_x >= num_rows || next_y < 0 || next_y >= num_cols) {
+      break;
+    }
+
+    if (map[next_x][next_y] == WALL) {
+
+      index_direction = (index_direction + 1) % 4;
+    } else {
+
+      x = next_x;
+      y = next_y;
+
+      if (!visited[x][y]) {
+        visited[x][y] = true;
+        add_guard_position++;
+      }
+    }
+
+    printf("Position actuelle : (%d, %d), direction : %d\n", x, y,
+           index_direction);
+  }
+
+  for (int i = 0; i < num_rows; i++) {
+    free(visited[i]);
+  }
+  free(visited);
+
+  return add_guard_position;
+}
+
+int main() {
+  FILE *file = NULL;
+  file = fopen("../input.txt", "r");
+  char line[2024];
+  int num_rows = 0;
+  int num_cols = 0;
+  char **map = NULL;
+
+  if (file != NULL) {
+    while (fgets(line, sizeof(line), file)) {
+      num_cols = strlen(line);
+      map = (char **)realloc(map, (num_rows + 1) * sizeof(char *));
+      map[num_rows] = strdup(line);
+      num_rows++;
+    }
+    fclose(file);
+  } else {
+    printf("Error opening file.\n");
+    return 1;
+  }
+
+  int result = guard_path(map, num_rows, num_cols);
+  printf("Result : %d\n", result);
+
+  for (int i = 0; i < num_rows; i++) {
+    free(map[i]);
+  }
+  free(map);
+
+  return 0;
+}
